1f75b728272ade51c52729f7d03312b6b3bdd688
Tom Aarts <taarts1@gmail.com>
2022-04-23 11:06:48 -0500

    ensure parity in python3 versus java monitoring programs

---
+++ assets/monitorGpio2.py
@@ -0,0 +1,81 @@
+
+#!/usr/bin/python3
+
+#* This is coded specifically to the manner in which the IS31FL3731 chip
+#* toggles the INTB pin.  Used in debugging the Pi OS missing short
+#* duration level changes on a GPIO
+#*        It Is NOT a generic monitor program.
+#*
+#* Sole purpose is unit testing.  Pi4 with Pi OS 64bit
+#* missing short duration interrupt. This program allows
+#* measuring the interrupt duration.
+
+
+import RPi.GPIO as GPIO
+import sys, getopt
+
+import time
+#from datetime import datetime, date, time
+
+
+
+
+def changed(pin):
+    print("Pin Low startTime : ",  float(round(time.time() * 1000)))
+
+def main(argv):
+    GPIO.setmode(GPIO.BCM)
+    btn = 42
+    obs_data = {'RA': "22:24:05.52" }
+    pred_data = {'RA':"22:24:05.60"}
+
+    print("!!!")
+    try:
+        opts, args = getopt.getopt(argv,"hp:o:",["pin=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('monitorGpio.py -p <pin>')
+            sys.exit()
+        elif opt in ("-p", "--pin"):
+            pin = int(arg)
+
+
+    print('Monitor pin "', pin)
+
+    btn = GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    val = GPIO.input(pin)
+    oldVal = 0
+    start_time = 0
+    end_time = 0
+    print("Initially value = ", val)
+    GPIO.add_event_detect(pin, GPIO.BOTH, callback=changed)
+
+    milliSecDelay = float(10/1000)
+    while(True):
+        time.sleep(milliSecDelay)
+
+    while False: #True:
+        GPIO.wait_for_edge(pin, GPIO.BOTH)
+        val = GPIO.input(pin)
+        print("Value = ", val)
+        if val != oldVal:
+            oldVal = val
+            if val == 1:
+                print("<1>State became  = ", val)
+                start_time = float(round(time.time() * 1000))
+            else:
+                print("<0>State became  = ", val)
+                end_time = float(round(time.time() * 1000))
+                break
+    print("out of while loop")
+    time_diff = (end_time - start_time)
+    print(" time diff ", time_diff)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- src/main/java/com/pi4j/devices/is31Fl37Matrix/README.md
+++ src/main/java/com/pi4j/devices/is31Fl37Matrix/README.md
@@ -49,6 +49,20 @@ com.pi4j.devices.multi/com.pi4j.devices.is31Fl37Matrix.MonitorInterrupt -p 18 -d
 Java Create output GPIO pin 18.  Create interrupt by driving line low for 15 millisecond duration 
 com.pi4j.devices.multi/com.pi4j.devices.is31Fl37Matrix.CreateInterrupt -p 18 -d LOW -m 15
 
+Python3 to create interrupt of specific interval
+python3 createInterrupt.py -p 18 -m 4 -d LOW
 
 Python3 Program knows the expected is31Fl37Matrix interrupt flow, measure duration of successfully processed interrupts
 python3 ./monitorGpio.py -p 25
+
+python3 monitorGpio2.py -p 25
+
+Pi-OS 64bit
+Monitor programs implemented in python3 process interrupts of 1 and 2 milliseconds.
+
+Monitor program implemented in java process interrupts of at least 10 milliseconds.
+
+The python3 program monitorGpio2.py uses the GPIO.add_event_detect function callback to more closely 
+operate same as the java monitor program's  event callback
+
+In either python3 monitor implementaiosn the required interrupt duration is ten times greater in java.
