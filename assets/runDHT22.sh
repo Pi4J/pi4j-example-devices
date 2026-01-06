9989e9363b12d97dde3503d5697e3fef718fcff6
Tom Aarts <taarts1@gmail.com>
2026-01-05 18:10:40 -1000

    modify sh files to work without being in modules

--- assets/runDHT22.sh
+++ assets/runDHT22.sh
@@ -1,37 +1,33 @@
 
+
 #
-#
-#
-#     *
-#     * -
-#     * #%L
-#     * **********************************************************************
-#     * ORGANIZATION  :  Pi4J
-#     * PROJECT       :  Pi4J :: EXTENSION
-#     * FILENAME      :  runDHT22.sh
-#     *
-#     * This file is part of the Pi4J project. More information about
-#     * this project can be found here:  https://pi4j.com/
-#     * **********************************************************************
-#     * %%
-#     *   * Copyright (C) 2012 - 2022 Pi4J
-#      * %%
-#     *
-#     * Licensed under the Apache License, Version 2.0 (the "License");
-#     * you may not use this file except in compliance with the License.
-#     * You may obtain a copy of the License at
-#     *
-#     *      http://www.apache.org/licenses/LICENSE-2.0
-#     *
-#     * Unless required by applicable law or agreed to in writing, software
-#     * distributed under the License is distributed on an "AS IS" BASIS,
-#     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     * See the License for the specific language governing permissions and
-#     * limitations under the License.
-#     * #L%
-#     *
-#
-#
+#   * #%L
+#   * Copyright (C) 2012 - 2025 Pi4J
+#  * %%
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *
+# *-
+# * #%L
+# * **********************************************************************
+# * ORGANIZATION  :  Pi4J
+# * PROJECT       :  Pi4J :: EXTENSION
+# * FILENAME      :  runDHT22.sh
+# *
+# * This file is part of the Pi4J project. More information about
+# * this project can be found here:  https://pi4j.com/
+# * **********************************************************************
+# * %%
 #
 #
 
@@ -39,4 +35,4 @@
 
 
 #!/usr/bin/env bash
-java --module-path . --module  com.pi4j.devices/com.pi4j.devices.dht22.DHT22_App  $@
+ java  -classpath ./*:../classes  com.pi4j.devices.dht22.DHT22_App  $@
